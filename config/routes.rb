e0b478d337af3c15ddd262808525932d0a971990
osha7 <ogroetz@gmail.com>
2023-01-10 21:44:00 +0000

    consolidate path

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
-  # get("/",  controller: => "movies", action: => "index" )
-  # get "/" => "movies#index"
-  root "movies#index"
-  # Routes for the Movie resource:
 
-  # CREATE
-  post "/movies" => "movies#create", as: :movies
-  get "/movies/new" => "movies#new", as: :new_movie
+  resource :movies
+  # # get("/",  controller: => "movies", action: => "index" )
+  # # get "/" => "movies#index"
+  # root "movies#index"
+  # # Routes for the Movie resource:
+
+  # # CREATE
+  # post "/movies" => "movies#create", as: :movies
+  # get "/movies/new" => "movies#new", as: :new_movie
           
-  # READ
-  get "/movies" => "movies#index"
-  # get "/movies/:id" => "movies#show", as: :details
-  get "/movies/:id" => "movies#show", as: :movie # movie() ===> expects an arg to populate :id segment
+  # # READ
+  # get "/movies" => "movies#index"
+  # # get "/movies/:id" => "movies#show", as: :details
+  # get "/movies/:id" => "movies#show", as: :movie # movie() ===> expects an arg to populate :id segment
   
-  # UPDATE
-  patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie() ===> expects an arg to populate :id segment
+  # # UPDATE
+  # patch "/movies/:id" => "movies#update"
+  # get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie() ===> expects an arg to populate :id segment
   
-  # DELETE
-  delete "/movies/:id" => "movies#destroy"
+  # # DELETE
+  # delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
