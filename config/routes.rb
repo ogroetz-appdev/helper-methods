90b63ebdaa29c7b6c62e65fbe9988d1c2871f6ed
osha7 <ogroetz@gmail.com>
2023-01-10 18:31:05 +0000

    routes and link_to helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "movies", :action => "index" })
-
+  # get("/",  controller: => "movies", action: => "index" )
+  # get "/" => "movies#index"
+  root "movies#index"
   # Routes for the Movie resource:
 
   # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-  get("/movies/new", { :controller => "movies", :action => "new" })
+  post "/movies" => "movies#create", as: :movies
+  get "/movies/new" => "movies#new", as: :new_movie
           
   # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies/:id", { :controller => "movies", :action => "show" })
+  get "/movies" => "movies#index"
+  # get "/movies/:id" => "movies#show", as: :details
+  get "/movies/:id" => "movies#show", as: :movie # movie() ===> expects an arg to populate :id segment
   
   # UPDATE
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  patch "/movies/:id" => "movies#update"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie() ===> expects an arg to populate :id segment
   
   # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
